810200c279e59a5b036c1019c701e89ce16fbd1b
igorgn <50628426+igorgn@users.noreply.github.com>
2021-10-13 16:22:37 +0300

    Update export_env.sh

--- .buildkite/export_env.sh
+++ .buildkite/export_env.sh
@@ -1,10 +1,12 @@
 export BRANCH=$BUILDKITE_BRANCH
-echo $BRANCH
+echo "BRANCH = $BRANCH"
+export RELEASE_BUILD=$(buildkite-agent meta-data get release-build)
+echo "RELEASE_BUILD = $RELEASE_BUILD"
 export VERSION=$(buildkite-agent meta-data get version)
-echo $VERSION
+echo "VERSION = $VERSION"
 export NPM_TAG=$(buildkite-agent meta-data get npm-tag)
-echo $NPM_TAG
+echo "NPM_TAG = $NPM_TAG"
 export BUILD_DOCUMENTATION_VERSION=$(buildkite-agent meta-data get build-documentation-version)
-echo $BUILD_DOCUMENTATION_VERSION
+echo "BUILD_DOCUMENTATION_VERSION = $BUILD_DOCUMENTATION_VERSION"
 export REMOVE_DOCUMENTATION_VERSION=$(buildkite-agent meta-data get remove-documentation-version)
-echo $REMOVE_DOCUMENTATION_VERSION
+echo "REMOVE_DOCUMENTATION_VERSION = $REMOVE_DOCUMENTATION_VERSION"
