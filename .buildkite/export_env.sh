5fa21fe3eec8a20992e2304b52ebcac199902e85
igorgn <50628426+igorgn@users.noreply.github.com>
2021-10-13 12:33:30 +0300

    Create export_env.sh

---
+++ .buildkite/export_env.sh
@@ -0,0 +1,8 @@
+export VERSION=$(buildkite-agent meta-data get version)
+echo $VERSION
+export NPM_TAG=$(buildkite-agent meta-data get npm-tag)
+echo $NPM_TAG
+export BUILD_DOCUMENTATION_VERSION=$(buildkite-agent meta-data get build-documentation-version)
+echo $BUILD_DOCUMENTATION_VERSION
+export REMOVE_DOCUMENTATION_VERSION=$(buildkite-agent meta-data get remove-documentation-version)
+echo $REMOVE_DOCUMENTATION_VERSION
